81d2f345405cf2c6a8a8db6998ba6fd9f712ce8d
Danny Qiu <dqiu55@gmail.com>
2015-05-21 21:03:26 -0400

    Add progress for Franklin

--- A-Very-Large-Haystack/solve.py
+++ A-Very-Large-Haystack/solve.py
@@ -1,19 +1,7 @@
 with open("text.txt") as f:
     data = f.read()
 
-    with open("modified2-1.txt", "w") as m:
-        for i in xrange(0, len(data), 2):
-            m.write(data[i])
+    every_other = data[::2] + data[1::2]
 
-    with open("modified2-2.txt", "w") as m:
-        for i in xrange(1, len(data), 2):
-            m.write(data[i])
-
-    with open("modified3-1.txt", "w") as m:
-        for i in xrange(1, len(data), 3):
-            m.write(data[i])
-
-    with open("modified4-1.txt", "w") as m:
-        for i in xrange(1, len(data), 4):
-            m.write(data[i])
-
+    with open("modified.txt", "w") as m:
+        m.write(every_other)

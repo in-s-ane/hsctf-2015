8e629d756d01920b7203313cf35c3d739bc01cc1
Danny Qiu <dqiu55@gmail.com>
2015-05-23 17:23:51 -0400

    Add progress to Keith's Ice Tree

--- Keith's-Ice-Tree_300/Solve.java
+++ Keith's-Ice-Tree_300/Solve.java
@@ -1,19 +1,22 @@
 import java.util.*;
 import java.io.*;
-import java.lang.Math;
 
 public class Solve {
+
+    public static Node[] nodeList;
+
     public static class Node {
-        public int id;
-        public int parent;
-        public int lChild;
-        public int rChild;
-        public long numSubtrees;
-        public Node(int _id, int _parent, int _lChild, int _rChild) {
-            id = _id;
-            parent = _parent;
-            lChild = _lChild;
-            rChild = _rChild;
+        int score;
+        int number;
+        int parent;
+        int left;
+        int right;
+        boolean checked = false;
+        public Node(int number) {
+            this.number = number;
+        }
+        public String toString() {
+            return this.left + "-<" + this.number + "(" + score + ")>-" + this.right;
         }
     }
 
@@ -21,74 +24,67 @@ public class Solve {
         BufferedReader in = new BufferedReader(new FileReader("input.txt"));
         String data = in.readLine();
         int numNodes = Integer.parseInt(data);
-        System.out.println("Num nodes: " + numNodes);
-        Node[] nodes = new Node[numNodes];
-        for (int i = 0; i < numNodes; ++i) {
-            nodes[i] = new Node(i+1, 0, 0, 0);
+        nodeList = new Node[numNodes+1];
+        for (int i = 1; i <= numNodes; i++) {
+            nodeList[i] = (new Node(i));
         }
-        for (int i = 0; i < numNodes; ++i) {
+        for (int i = 1; i <= numNodes; i++) {
             String line = in.readLine();
-            String[] lineData = line.split(" ");
-            int p = Integer.parseInt(lineData[0]);
-            int l = Integer.parseInt(lineData[1]);
-            int r = Integer.parseInt(lineData[2]);
-            if (l != 0) {
-                nodes[p-1].lChild = l;
-                nodes[l-1].parent = p;
+            String[] nodeData = line.split(" ");
+            int number = Integer.parseInt(nodeData[0]);
+            int left = Integer.parseInt(nodeData[1]);
+            int right = Integer.parseInt(nodeData[2]);
+            nodeList[number].left = left;
+            nodeList[number].right = right;
+            if (left != 0) {
+                nodeList[left].parent = number;
             }
-            if (r != 0) {
-                nodes[p-1].rChild = r;
-                nodes[r-1].parent = p;
-            }
-            nodes[p-1].numSubtrees = 1;
-        }
-        int rootNode = 0;
-        for (int i = 0; i < numNodes; ++i) {
-            if (nodes[i].parent == 0) {
-                rootNode = i+1;
-                System.out.println("Found root: " + Integer.toString(i+1));
+            if (right != 0) {
+                nodeList[right].parent = number;
             }
         }
-        int depth = 0;
-        for (int i = 0; i < numNodes; ++i) {
-            Node curr = nodes[i];
-            if (curr.lChild == 0 && curr.rChild == 0) { // Leaf
-                int nodeDepth = 0;
-                while (curr.parent != 0) { // While it's not the root node
-                    curr = nodes[curr.parent-1];
-                    ++nodeDepth;
-                }
-                depth = Math.max(depth, nodeDepth);
-            }
-        }
-        System.out.println("Tree depth: " + Integer.toString(depth));
-        //for (int passes = 0; passes < numNodes; ++passes) {
-        //    for (int i = 0; i < numNodes; ++i) {
-        //        Node curr = nodes[i];
-        //        curr.numSubtrees = 1;
-        //        if (curr.lChild != 0) {
-        //            curr.numSubtrees += 1;
-        //            curr.numSubtrees += nodes[curr.lChild-1].numSubtrees;
-        //        }
-        //        if (curr.rChild != 0) {
-        //            curr.numSubtrees += 1;
-        //            curr.numSubtrees += nodes[curr.rChild-1].numSubtrees;
-        //        }
-        //        if (curr.lChild != 0 && curr.rChild != 0) {
-        //            curr.numSubtrees += 1;
-        //        }
-        //    }
-        //}
-        //System.out.println("Subtrees of root: " + Long.toString(nodes[rootNode-1].numSubtrees));
-        //System.out.println("Subtrees of root.left: " + Long.toString(nodes[nodes[rootNode-1].lChild-1].numSubtrees));
-        //System.out.println("Subtrees of root.right: " + Long.toString(nodes[nodes[rootNode-1].rChild-1].numSubtrees));
-        ////long ans = 1;
-        ////for (int i = 0; i < numNodes; ++i) {
-        ////    //System.out.println(Long.toString(nodes[i].numSubtrees));
-        ////    ans *= nodes[i].numSubtrees;
-        ////    ans %= 1000000007;
-        ////}
-        ////System.out.println("Total: " + Long.toString(ans));
+        long answer = scoreNodes(nodeList);
+        System.out.println("Total number of connected subtrees: " + answer);
+        System.out.println("Total number of connected subtrees mod 1000000007: " + answer % 1000000007);
+        //System.out.println(Arrays.toString(nodeList));
     }
-}
 
+    public static long scoreNodes(Node[] nodeList) {
+        ArrayList<Node> temp = new ArrayList<Node>();
+        for (int i=1; i<nodeList.length; i++) {
+            if (nodeList[i].left == 0 && nodeList[i].right == 0) {
+                temp.add(nodeList[i]);
+            }
+        }
+        while (!temp.isEmpty()) {
+            int score = 1;
+            Node cur = temp.remove(0);
+            if (cur != null) {
+                if ((cur.left != 0 && !nodeList[cur.left].checked) || (cur.right != 0 && !nodeList[cur.right].checked)) {
+                    temp.add(cur);
+                    continue;
+                }
+                if (cur.left != 0 && cur.right != 0) {
+                    score += nodeList[cur.left].score * nodeList[cur.right].score;
+                }
+                if (cur.left != 0) {
+                    score += nodeList[cur.left].score;
+                }
+                if (cur.right != 0) {
+                    score += nodeList[cur.right].score;
+                }
+                cur.score = score;
+                cur.checked = true;
+                if (!temp.contains(nodeList[cur.parent])) {
+                    temp.add(nodeList[cur.parent]);
+                }
+            }
+        }
+        long total = 0;
+        for (int i=1; i<nodeList.length; i++) {
+            total += nodeList[i].score;
+        }
+        return total;
+    }
+
+}
